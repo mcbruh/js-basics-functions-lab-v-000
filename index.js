6024f59d53998d3a9e7b9e1443d54bc1e201690f
Michael Chen <michael.chen.1120@gmail.com>
2018-12-29 02:00:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(street) {
   let blocks = street - 42
-  return blocks;
+  return Math.abs(blocks);
 };
 
 function distanceFromHqInFeet(street) {

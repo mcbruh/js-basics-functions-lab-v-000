238d41c1f4e592cc06d66f6fb64ae6061c7484fe
Michael Chen <michael.chen.1120@gmail.com>
2018-12-29 02:15:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,4 +21,10 @@ function calculatesFarePrice(start, finish) {
   else if (distance > 400 && distance < 2000) {
     return ((distance - 400) * .02)
   }
+  else if (distance > 2000 && distance < 2500) {
+    return 25
+  }
+  else {
+    return "cannot travel that far"
+  }
 }

48323702d842f4404acc5394c8d97c80ab2d1861
Michael Chen <michael.chen.1120@gmail.com>
2018-12-29 01:56:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function distanceFromHqInBlocks(street) {
   const block = 264
   let distance = street - 42
   let blocks = distance / block
-  Math.ceil(blocks);
+  return Math.ceil(blocks);
 };
 
 function distanceFromHqInFeet() {

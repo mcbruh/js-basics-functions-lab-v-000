97c80d94d67376e5c665e2b6a6390f0ccf0efcfd
Michael Chen <michael.chen.1120@gmail.com>
2018-12-29 01:41:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(distance) {
   const block = 264
-  return Math.ceiling((distance / blocks))
+  return Math.ceil((distance / block))
 };

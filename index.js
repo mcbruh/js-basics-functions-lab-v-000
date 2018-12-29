d975acad957f4c925a7ca156001284ed48c79f74
Michael Chen <michael.chen.1120@gmail.com>
2018-12-29 02:10:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function distanceTravelledInFeet(start, finish) {
 function calculatesFarePrice(start, finish) {
   let distance = distanceTravelledInFeet(start, finish)
   if (distance < 400) {
-    0
+    return 0
   }
 }
